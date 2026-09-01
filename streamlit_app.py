dd3978dee7e79ae004406a83bf33afb07e90bcd0
Zblah <s.gompel@biocodex.fr>
2026-09-01 16:22:43 +0200

    Fix column name in DataFrame selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Get fruit options
-my_dataframe = (session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH ON')))
+my_dataframe = (session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON')))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
