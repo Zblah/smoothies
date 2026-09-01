55bac903280d23fc8fdbf6463560ea3ddf7453da
Zblah <s.gompel@biocodex.fr>
2026-09-01 14:46:38 +0200

    Create smoothie customization Streamlit app
    This Streamlit app allows users to customize their smoothie by selecting fruits and entering their name. It connects to a Snowflake database to retrieve fruit options and submits the order to the database.

---
+++ streamlit_app.py
@@ -0,0 +1,63 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# App title
+st.title("🥤 Customize Your Smoothie! 🥤")
+
+st.write(
+    """
+    Choose the fruits you want in your smoothie!
+    """
+)
+
+# Name on order
+name_on_order = st.text_input("Name on smoothie:")
+
+st.write("The name on your smoothie will be", name_on_order)
+
+# Connect to Snowflake
+session = get_active_session()
+
+# Get fruit options
+my_dataframe = (
+    session
+    .table("smoothies.public.fruit_options")
+    .select(col("FRUIT_NAME"))
+)
+
+# Fruit selection
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe
+    , max_selections=5
+)
+
+if ingredients_list:
+
+    ingredients_string = ""
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
+
+    # Build INSERT statement
+    my_insert_stmt = """
+        INSERT INTO smoothies.public.orders
+        (ingredients, name_on_order)
+        VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')
+    """
+
+    # Optional: display SQL while testing
+    #st.write(my_insert_stmt)
+    #st.stop()
+    
+    time_to_insert = st.button("Submit Order")
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+
+        st.success(
+            f"Your Smoothie is ordered, {name_on_order}!",
+            icon="✅"
+        )
