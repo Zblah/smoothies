f61f8333c800794f031cd6aa5e876c1014020b5e
Zblah <s.gompel@biocodex.fr>
2026-09-01 15:00:38 +0200

    Change Snowflake session connection to Streamlit
    Updated connection method to use Streamlit's connection instead of Snowflake's active session.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # App title
@@ -17,8 +16,9 @@ name_on_order = st.text_input("Name on smoothie:")
 
 st.write("The name on your smoothie will be", name_on_order)
 
-# Connect to Snowflake
-session = get_active_session()
+# Connect to Streamlit
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Get fruit options
 my_dataframe = (
